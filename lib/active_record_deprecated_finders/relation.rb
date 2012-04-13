2a97f34c58a51e2882f7546101119fa25d814757
Jon Leighton <j@jonathanleighton.com>
2012-04-13 19:06:08 +0100

    import deprecated find_in_batches code

--- lib/active_record_deprecated_finders/relation.rb
+++ lib/active_record_deprecated_finders/relation.rb
@@ -1,38 +1,55 @@
+require 'active_record/relation'
 require 'active_support/core_ext/module/aliasing'
 
 module ActiveRecord
   class Relation
-    VALID_FIND_OPTIONS = [ :conditions, :include, :joins, :limit, :offset, :extend,
-                           :order, :select, :readonly, :group, :having, :from, :lock ]
+    module DeprecatedMethods
+      VALID_FIND_OPTIONS = [ :conditions, :include, :joins, :limit, :offset, :extend,
+                             :order, :select, :readonly, :group, :having, :from, :lock ]
 
-    def apply_finder_options(options)
-      relation = clone
-      return relation unless options
+      def apply_finder_options(options)
+        relation = clone
+        return relation unless options
 
-      options.assert_valid_keys(VALID_FIND_OPTIONS)
-      finders = options.dup
-      finders.delete_if { |key, value| value.nil? && key != :limit }
+        options.assert_valid_keys(VALID_FIND_OPTIONS)
+        finders = options.dup
+        finders.delete_if { |key, value| value.nil? && key != :limit }
 
-      ((VALID_FIND_OPTIONS - [:conditions, :include, :extend]) & finders.keys).each do |finder|
-        relation = relation.send(finder, finders[finder])
+        ((VALID_FIND_OPTIONS - [:conditions, :include, :extend]) & finders.keys).each do |finder|
+          relation = relation.send(finder, finders[finder])
+        end
+
+        relation = relation.where(finders[:conditions]) if options.has_key?(:conditions)
+        relation = relation.includes(finders[:include]) if options.has_key?(:include)
+        relation = relation.extending(finders[:extend]) if options.has_key?(:extend)
+
+        relation
       end
 
-      relation = relation.where(finders[:conditions]) if options.has_key?(:conditions)
-      relation = relation.includes(finders[:include]) if options.has_key?(:include)
-      relation = relation.extending(finders[:extend]) if options.has_key?(:extend)
+      def update_all_with_deprecated_options(updates, conditions = nil, options = {})
+        if conditions || options.present?
+          where(conditions)
+            .apply_finder_options(options.slice(:limit, :order))
+            .update_all_without_deprecated_options(updates)
+        else
+          update_all_without_deprecated_options(updates)
+        end
+      end
 
-      relation
-    end
+      def find_in_batches(options = {}, &block)
+        if (finder_options = options.except(:start, :batch_size)).present?
+          raise "You can't specify an order, it's forced to be #{batch_order}" if options[:order].present?
+          raise "You can't specify a limit, it's forced to be the batch_size"  if options[:limit].present?
 
-    def update_all_with_deprecated_options(updates, conditions = nil, options = {})
-      if conditions || options.present?
-        where(conditions)
-          .apply_finder_options(options.slice(:limit, :order))
-          .update_all_without_deprecated_options(updates)
-      else
-        update_all_without_deprecated_options(updates)
+          apply_finder_options(finder_options).
+            find_in_batches(options.slice(:start, :batch_size), &block)
+        else
+          super
+        end
       end
     end
+
+    include DeprecatedMethods
     alias_method_chain :update_all, :deprecated_options
   end
 end
--- test/update_all_test.rb
+++ test/update_all_test.rb
@@ -1,11 +1,15 @@
 require 'helper'
 
 describe 'update_all' do
+  after do
+    Post.destroy_all
+  end
+
   it 'supports conditions' do
     foo = Post.create title: 'foo'
     bar = Post.create title: 'bar'
 
-    Post.where(id: [foo, bar]).update_all({ title: 'omg' }, title: 'foo')
+    Post.update_all({ title: 'omg' }, title: 'foo')
 
     foo.reload.title.must_equal 'omg'
     bar.reload.title.must_equal 'bar'
@@ -13,7 +17,7 @@ describe 'update_all' do
 
   it 'supports limit and order' do
     posts = 3.times.map { Post.create }
-    Post.where(id: posts).update_all({ title: 'omg' }, {}, limit: 2, order: :id)
+    Post.update_all({ title: 'omg' }, {}, limit: 2, order: :id)
 
     posts.each(&:reload).map(&:title).must_equal ['omg', 'omg', nil]
   end
