dbdb493d823adad6535bc3c434a950483a8b1760
Jon Leighton <j@jonathanleighton.com>
2012-04-22 13:13:05 +0200

    #with_scope and #with_exclusive_scope

--- lib/active_record_deprecated_finders/base.rb
+++ lib/active_record_deprecated_finders/base.rb
@@ -50,6 +50,73 @@ module ActiveRecord
 
       super
     end
+
+    def with_scope(scope = {}, action = :merge)
+      # If another Active Record class has been passed in, get its current scope
+      scope = scope.current_scope if !scope.is_a?(Relation) && scope.respond_to?(:current_scope)
+
+      previous_scope = self.current_scope
+
+      if scope.is_a?(Hash)
+        # Dup first and second level of hash (method and params).
+        scope = scope.dup
+        scope.each do |method, params|
+          scope[method] = params.dup unless params == true
+        end
+
+        scope.assert_valid_keys([ :find, :create ])
+        relation = construct_finder_arel(scope[:find] || {})
+        relation.default_scoped = true unless action == :overwrite
+
+        if previous_scope && previous_scope.create_with_value && scope[:create]
+          scope_for_create = if action == :merge
+            previous_scope.create_with_value.merge(scope[:create])
+          else
+            scope[:create]
+          end
+
+          relation = relation.create_with(scope_for_create)
+        else
+          scope_for_create = scope[:create]
+          scope_for_create ||= previous_scope.create_with_value if previous_scope
+          relation = relation.create_with(scope_for_create) if scope_for_create
+        end
+
+        scope = relation
+      end
+
+      scope = previous_scope.merge(scope) if previous_scope && action == :merge
+      scope.scoping { yield }
+    end
+
+    protected
+
+    # Works like with_scope, but discards any nested properties.
+    def with_exclusive_scope(method_scoping = {}, &block)
+      if method_scoping.values.any? { |e| e.is_a?(ActiveRecord::Relation) }
+        raise ArgumentError, <<-MSG
+New finder API can not be used with_exclusive_scope. You can either call unscoped to get an anonymous scope not bound to the default_scope:
+
+User.unscoped.where(:active => true)
+
+Or call unscoped with a block:
+
+User.unscoped do
+User.where(:active => true).all
+end
+
+MSG
+      end
+      with_scope(method_scoping, :overwrite, &block)
+    end
+
+    private
+
+    def construct_finder_arel(options = {}, scope = nil)
+      relation = options.is_a?(Hash) ? unscoped.apply_finder_options(options) : options
+      relation = scope.merge(relation) if scope
+      relation
+    end
   end
 
   class Base
---
+++ test/with_scope_test.rb
@@ -0,0 +1,26 @@
+require 'helper'
+
+describe 'with_scope' do
+  before do
+    Post.create(id: 1, title: 'foo')
+    Post.create(id: 2, title: 'bar')
+  end
+
+  after do
+    Post.delete_all
+  end
+
+  it 'applies a scoping' do
+    Post.with_scope(find: { conditions: { title: 'foo' } }) do
+      assert_equal [1], Post.all.map(&:id)
+    end
+  end
+
+  it 'applies an exclusive scoping' do
+    Post.with_scope(find: { conditions: { title: 'foo' } }) do
+      Post.send(:with_exclusive_scope, find: { conditions: { title: 'bar' } }) do
+        assert_equal [2], Post.all.map(&:id)
+      end
+    end
+  end
+end
